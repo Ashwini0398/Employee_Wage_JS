ef36ea52cde22d20ab729890886f925058211490
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-03-15 22:00:24 +0530

    ADDED: employeewage

---
+++ employeeWage.js
@@ -0,0 +1,14 @@
+//Uc1 employee is present or absent
+{
+    const IS_ABSENT = 0;
+    let empCheck = Math.floor(Math.random() * 10 ) % 2;
+    if(empCheck == IS_ABSENT)
+    {
+        console.log("UC1-Employee is ABSENT");
+
+    }
+    else{
+        console.log("UC1-Employee is PRESENT");
+    }
+}
+
