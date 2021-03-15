d455784a5da9d67c9ff379d2374539c92e103b21
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-03-15 22:24:29 +0530

    ADDED: employee wage for working days in a month

--- employeeWage.js
+++ employeeWage.js
@@ -1,3 +1,4 @@
+
     const IS_PART_TIME = 1;
     const IS_FULL_TIME = 2;
     const PART_TIME_HOUR = 4;
@@ -5,25 +6,33 @@
     const WAGE_PER_HOUR = 20;
     const WORKING_HOURS = 20;
     {
+        
+
         function getWorkingHours(empCheck)
         {
-            switch(empCheck)
+           switch(empCheck)
             {
                 case IS_PART_TIME:
                     return empHrs = PART_TIME_HOUR;
-            
+                
                 case IS_FULL_TIME:
                     return empHrs = FULL_TIME_HOUR;
-                default:
+                                default:
                     return 0;
-        }
-    }
-        let empHrs = 0;
-        let empCheck = Math.floor(Math.random()*10) % 3;
-        empHrs = getWorkingHours(empCheck); 
-        let empWage = empHrs * WAGE_PER_HOUR;
-        console.log("Emp Wage: "+empWage);
-    }
 
+            }
+        }
+        
+        
+
+        let totalEmpHrs = 0;
+        for (let day = 0 ; day < WORKING_HOURS; day++)
+        {
+            let empCheck = Math.floor(Math.random()*10) % 3;
+            totalEmpHrs += getWorkingHours(empCheck);
+        }
+        let empWage = totalEmpHrs * WAGE_PER_HOUR;
+        console.log("EmployeeHours: "+ totalEmpHrs +"Emp Wage: "+empWage);
+    }
 
 
