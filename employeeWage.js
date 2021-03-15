9580694bdadcd5a54b132d8972ea97deebb948f2
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-03-15 22:20:25 +0530

    REFACTRED: employee daily wage using function

--- employeeWage.js
+++ employeeWage.js
@@ -3,24 +3,26 @@
     const PART_TIME_HOUR = 4;
     const FULL_TIME_HOUR = 8;
     const WAGE_PER_HOUR = 20;
+    const WORKING_HOURS = 20;
     {
-        let empHrs =0;
-        let empCheck = Math.floor(Math.random()*10) % 3;
-        switch(empCheck)
+        function getWorkingHours(empCheck)
         {
-            case IS_PART_TIME:
-                empHrs = PART_TIME_HOUR;
-                break;
-            case IS_FULL_TIME:
-                empHrs = FULL_TIME_HOUR;
-                break;
-            default:
-                empHrs= 0;
+            switch(empCheck)
+            {
+                case IS_PART_TIME:
+                    return empHrs = PART_TIME_HOUR;
+            
+                case IS_FULL_TIME:
+                    return empHrs = FULL_TIME_HOUR;
+                default:
+                    return 0;
         }
-    
+    }
+        let empHrs = 0;
+        let empCheck = Math.floor(Math.random()*10) % 3;
+        empHrs = getWorkingHours(empCheck); 
         let empWage = empHrs * WAGE_PER_HOUR;
         console.log("Emp Wage: "+empWage);
-
     }
 
 
