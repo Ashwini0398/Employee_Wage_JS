83ccde145f97288310a57b46d7ba1868d018c8a4
Ashwini Khonde <ashwinikhonde98@gmail.com>
2021-03-16 19:19:46 +0530

    REFACTORED: total working days and hours in month using array

--- employeeWage.js
+++ employeeWage.js
@@ -5,11 +5,10 @@
     const FULL_TIME_HOUR = 8;
     const WAGE_PER_HOUR = 20;
     const WORKING_HOURS = 20;
-    const MAX_HR_IN_MONTH = 100;
-    const NUM_OF_WORKING_DAYS =10;
+    const MAX_HR_IN_MONTH = 160;
+    const NUM_OF_WORKING_DAYS =20;
+    
     {
-        
-        
         function getWorkingHours(empCheck)
         {
            switch(empCheck)
@@ -21,19 +20,26 @@
                     return empHrs = FULL_TIME_HOUR;
                                 default:
                     return 0;
-
             }
         }
-        
+
+        function calDailyWage(empHrs)
+        {
+            return empHrs * WAGE_PER_HOUR;
+        }
+
         let totalEmpHrs = 0;
         let totalWorkingDays = 0;
+        let empDailyWAgeArr = new Array();
         while(totalEmpHrs <= MAX_HR_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
             totalWorkingDays++;
             let empCheck = Math.floor(Math.random()*10) % 3;
             totalEmpHrs += getWorkingHours(empCheck);
+            empDailyWAgeArr.push(calDailyWage(totalEmpHrs));
         }
-        let empWage = totalEmpHrs * WAGE_PER_HOUR;
-        console.log("Total Days: " + totalWorkingDays +" Employee Hours: " + totalEmpHrs +"Emp Wage: "+empWage);
+
+        let empWage = calDailyWage(totalEmpHrs);
+        console.log("Total Days: " + totalWorkingDays +" Employee Hours: " + totalEmpHrs +" Emp Wage: "+ empWage);
     }
 
 
